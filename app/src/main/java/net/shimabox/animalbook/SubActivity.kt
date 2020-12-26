c19aa6bf7834bd89ea5360a0568b716bb2583d5a
shimabox <shimabox@gmail.com>
2020-12-26 12:25:54 +0900

    :sparkles: ボタンをタップしてフラグメントを表示する処理

--- app/src/main/java/net/shimabox/animalbook/SubActivity.kt
+++ app/src/main/java/net/shimabox/animalbook/SubActivity.kt
@@ -2,10 +2,47 @@ package net.shimabox.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.shimabox.animalbook.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubBinding
+    private lateinit var title: TitleFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+        binding = ActivitySubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lionButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, LionFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.hippoButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, HippoFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        binding.giraffeButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, GiraffeFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        title = TitleFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.titleFrame, title)
+            commit()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        title.setTitle("サブ画面")
     }
 }
