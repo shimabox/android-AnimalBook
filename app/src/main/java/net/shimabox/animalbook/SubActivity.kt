96185fdfea7ac2bf0e7d9b4938715225ed9f190a
shimabox <shimabox@gmail.com>
2020-12-26 11:02:04 +0900

    :sparkles: Add SubActivity.

---
+++ app/src/main/java/net/shimabox/animalbook/SubActivity.kt
@@ -0,0 +1,11 @@
+package net.shimabox.animalbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
