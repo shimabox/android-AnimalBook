7ea1962291ef0832e27e91a703112dd37b89b2cc
shimabox <shimabox@gmail.com>
2020-12-26 12:37:10 +0900

    :sparkles: サブ画面(動物図鑑)への遷移用ボタンを配置

--- app/src/main/java/net/shimabox/animalbook/MainActivity.kt
+++ app/src/main/java/net/shimabox/animalbook/MainActivity.kt
@@ -1,11 +1,20 @@
 package net.shimabox.animalbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.shimabox.animalbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
